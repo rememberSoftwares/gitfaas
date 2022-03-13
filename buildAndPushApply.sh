bc35580903e2ad0c5390e85c89735ef87d89267f
docteur <rs.alpha14.com>
2022-03-13 23:36:54 +0100

    fixed buildAndPush to point on docker hub

--- buildAndPushApply.sh
+++ buildAndPushApply.sh
@@ -1,4 +1,4 @@
 cd ./applier
-docker build . -t apply:$1
-docker tag $1 wingardiumleviosa/gitfaas/apply:$1
-docker push wingardiumleviosa/gitfaas/apply:$1
+docker build . -t wingardiumleviosa/gitfaas-apply:$1
+docker tag gitfaas-apply:$1 wingardiumleviosa/gitfaas-apply:$1
+docker push wingardiumleviosa/gitfaas-apply:$1

de597818e8c5720d0f95720734cc93d93b5f43e8
docteur <rs.alpha14.com>
2023-12-18 00:02:54 +0100

    repo name is now configurable in values.yaml

--- buildAndPushApply.sh
+++ buildAndPushApply.sh
@@ -1,5 +1,5 @@
 cd ./applier
-docker build . -t wingardiumleviosa/gitfaas-apply:$1
-docker tag gitfaas-apply:$1 wingardiumleviosa/gitfaas-apply:$1
-docker push wingardiumleviosa/gitfaas-apply:$1
+docker build . -t petrificustotalus/gitfaas-apply:$1
+docker tag gitfaas-apply:$1 petrificustotalus/gitfaas-apply:$1
+docker push petrificustotalus/gitfaas-apply:$1
 echo "$1" >> bap_apply
--- buildAndPushGit.sh
+++ buildAndPushGit.sh
@@ -1,5 +1,5 @@
 cd ./git
-docker build . -t wingardiumleviosa/gitfaas-git:$1
-docker tag gitfaas-git:$1 wingardiumleviosa/gitfaas-git:$1
-docker push wingardiumleviosa/gitfaas-git:$1
+docker build . -t petrificustotalus/gitfaas-git:$1
+docker tag gitfaas-git:$1 petrificustotalus/gitfaas-git:$1
+docker push petrificustotalus/gitfaas-git:$1
 echo "$1" > bap_git
